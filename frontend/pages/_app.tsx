de30827a5feb0ca6945b38bc7369de5d698f6a1b
SeijaHuang <hsj12544975@gmail.com>
2024-09-07 10:26:24 +1000

    initial chakra

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,16 @@
 import Navigation from "@/components/homepage/layout/Navigation";
 import "@/styles/globals.css";
+import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const theme = extendBaseTheme({});
+
   return (
-    <Navigation>
-      <Component {...pageProps} />
-    </Navigation>
+    <ChakraBaseProvider theme={theme}>
+      <Navigation>
+        <Component {...pageProps} />
+      </Navigation>
+    </ChakraBaseProvider>
   );
 }
