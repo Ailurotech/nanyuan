0b234d6dfa1ce17bf9d1d75f6d8a7d2f1127e3e5
Depeng Sun <shangguming@gmail.com>
2024-09-09 11:12:39 +0930

    2 homepage (#15)
    * homepage implement with Sanity IO

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,46 @@
-import { Inter } from "next/font/google";
+import HomePage from "../components/homepage/HomePage"; 
+import { GetStaticProps } from "next";
+import { sanityClient } from "@/lib/sanityClient";
+import { HomePageContent as HomePageContentType } from "@/types"; 
 
-const inter = Inter({ subsets: ["latin"] });
+interface IndexProps {
+  homePageContent: HomePageContentType | null;
+}
 
-export default function Home() {
+export default function Index({ homePageContent }: IndexProps) { 
   return (
     <div>
-      <div>Home Page</div>
+      <HomePage homePageContent={homePageContent} />
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const query = `
+    *[_type == "HomePageContent"][0]{
+      Homepagetitle,
+      backgroundimg{
+        asset->{
+          url
+        }
+      },
+      dishimg{
+        asset->{
+          url
+        }
+      },
+      cheftext,
+      chefname
+    }
+  `;
+
+  const homePageContent = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      homePageContent,
+    },
+    revalidate: 86400,
+  };
+};
+
