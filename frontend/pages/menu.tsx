b43bdf8a96b26e0e76934f3fe8312267b3d42e38
Kaifeng Yao <kaifeng.yao@hotmail.com>
2024-09-15 12:04:34 +0930

    hover-interactions

--- frontend/pages/menu.tsx
+++ frontend/pages/menu.tsx
@@ -14,7 +14,7 @@ const MenuPage = ({ menuItems }: MenuProps) => {
       <h1 className="text-center text-white text-4xl font-bold mb-8">Choose Our Menu</h1>
       <div className="container mx-auto grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-5 mt-5 justify-items-center px-5">
         {menuItems.map((item) => (
-          <Card key={item._id} sx={{ backgroundColor: "#191919" }} maxW="300px" className="shadow-lg rounded-lg p-4">
+          <Card key={item._id} sx={{ backgroundColor: "#191919" }} maxW="300px" className="shadow-lg rounded-lg p-4 transform transition-transform duration-300 hover:scale-105">
             <CardBody className="relative" justifyContent="center"  >
                 <Image
                   src={item.image}
@@ -44,6 +44,7 @@ const MenuPage = ({ menuItems }: MenuProps) => {
                 display="flex"
                 justifyContent="center"
                 alignItems="center"
+                _hover={{ bg: "black", color: "white" }}
               >
                 +
               </Button>
