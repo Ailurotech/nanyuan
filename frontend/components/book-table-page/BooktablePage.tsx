6b33cd2d13ee0580d6ea1db86fba37309ddbaa72
Shunyang Zhai <a421104346@gmail.com>
2025-03-14 03:35:18 +1030

    resolve conflicts

--- frontend/components/book-table-page/BooktablePage.tsx
+++ frontend/components/book-table-page/BooktablePage.tsx
@@ -112,13 +112,19 @@ export function BooktablePage({ restaurant, table }: BooktablePageProps) {
           ),
       ]);
 
-      const apiUrl = process.env.NEXT_PUBLIC_CREATE_RESERVATION_URL || '';
-      if (!apiUrl) throw new Error('API URL is missing');
-      const { data: result } = await axios.post(apiUrl, {
-        data,
-        tableId: validationResult.tableId,
-      });
+      const createReservationData = {
+        _type: 'reservation',
+        time: `${data.date}T${data.time}`,
+        table: { _type: 'reference', _ref: validationResult.tableId },
+        name: data.name,
+        phone: data.phone,
+        email: data.email,
+        guests: data.guests,
+        preference: data.preference,
+        notes: data.notes,
+      };
 
+      await axios.post('/api/createReservations', createReservationData);
       setSuccessMessage(
         `Your table is booked on ${data.date} at ${data.time}. Enjoy your meal!`,
       );
